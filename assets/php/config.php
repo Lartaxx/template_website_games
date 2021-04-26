e8b38c7f323ba722289b2aadf803a9cece9a3593
David <34965385+Lartaxx@users.noreply.github.com>
2021-04-26 18:29:26 +0200

    Launch commit

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration for game query
 define("type", "gmod"); // You have all servers here https://github.com/Austinb/GameQ/tree/v3/src/GameQ/Protocols
-define("host", "51.38.243.116"); // The host of your server (without the port!)
-define("port", "27310"); // The port of your server
+define("host", "151.80.111.34"); // The host of your server (without the port!)
+define("port", "27015"); // The port of your server
 ?>
